d49b052aff5707b61885c3f5b0ea34cb5cd4a04b
ikayaki-bit <ikayaki.koge@gmail.com>
2025-06-27 12:30:00 +0900

    Won/lost message added

--- dice_game.c
+++ dice_game.c
@@ -3,8 +3,13 @@
 #include <time.h>
 
 int main(){
+    char str[15];
     int die1, die2;
     int total=0;
+
+    printf("What is your name?\n> ");
+    scanf("%s",str);
+    printf("Hello, %s!\n",str);
 
     srand((unsigned) time(NULL));
 
@@ -15,4 +20,13 @@ int main(){
     total = die1 + die2;
 
     printf("Die 1: %d\nDie 2: %d\nTotal value: %d\n",die1, die2, total);
+
+    if(total > 7){
+        printf("You won!\n");
+    }
+    else {
+        printf("You lost!\n");
+    }
+
+    return 0;
 }
